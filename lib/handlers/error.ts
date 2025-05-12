17dc52d56eacc72752dd263b74a19830661e0ec5
Sandeep <sandeepdewangan7@gmail.com>
2025-05-12 11:13:26 +0530

    Error Handling

---
+++ lib/handlers/error.ts
@@ -0,0 +1,87 @@
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+import { RequestError, ValidationError } from "../http-errors";
+
+export type ResponseType = "api" | "server";
+
+const formatResponse = (
+    responseType: ResponseType,
+    status: number,
+    message: string,
+    errors?: Record<string, string[]> | undefined
+) => {
+    const responseContent = {
+        success: false,
+        error: {
+            message,
+            details: errors,
+        },
+    };
+
+    return responseType === "api"
+        ? NextResponse.json(responseContent, { status })
+        : { status, ...responseContent };
+};
+
+const handleError = (error: unknown, responseType: ResponseType = "server") => {
+    if (error instanceof RequestError) {
+        return formatResponse(
+            responseType,
+            error.statusCode,
+            error.message,
+            error.errors
+        );
+    }
+
+    if (error instanceof ZodError) {
+        const validationError = new ValidationError(
+            error.flatten().fieldErrors as Record<string, string[]>
+        );
+
+        return formatResponse(
+            responseType,
+            validationError.statusCode,
+            validationError.message,
+            validationError.errors
+        );
+    }
+
+    if (error instanceof Error) {
+        return formatResponse(responseType, 500, error.message);
+    }
+
+    return formatResponse(responseType, 500, "An unexpected error occurred");
+};
+
+export default handleError;
+
+/*
+    throw new Error("Test Error"); // Generic Error
+    throw new NotFoundError("Home"); // Page not found error
+
+    O/p:
+    
+    {
+      status: 404,
+      success: false,
+      error: { message: 'Home not found', details: undefined }
+    }
+ 
+    throw new ValidationError({
+      title: ["Required", "Title cannot start with number"],
+      tags: ["Javasrpt is not a valid tag"],
+    }); // Validation Error
+    
+    O/p:
+    
+    {
+      status: 400,
+      success: false,
+      error: {
+              message: 'Title is required., Javasrpt is not a valid tag',
+              details: { title: [Array], tags: [Array] }
+            }
+    }
+    
+*/
